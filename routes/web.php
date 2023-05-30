845a20ae60ff281b4dfd3bea4824ed673785e3e5
marcellitovt <marcellitoya@gmail.com>
2023-05-31 05:26:32 +0700

    Perubahan Bahasa V1

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => ['role:superadmin|admin|cashier']], function () {
+Route::group(['middleware' => ['role:superadmin|admin|cashier', 'token.expired']], function () {
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
     Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
     Route::post('/cart/change-qty', [CartController::class, 'changeQty']);
@@ -27,7 +27,7 @@ Route::group(['middleware' => ['role:superadmin|admin|cashier']], function () {
     Route::resource('orders', OrderController::class)->only(['index', 'show', 'create', 'store']);
 });
 
-Route::group(['middleware' => ['role:superadmin|admin|inventory']], function () {
+Route::group(['middleware' => ['role:superadmin|admin|inventory', 'token.expired']], function () {
     Route::resource('categories', CategoryController::class);
     Route::get('/inventory', [ProductController::class, 'index'])->name('inventory.index');
     Route::resource('products', ProductController::class);
@@ -35,13 +35,13 @@ Route::group(['middleware' => ['role:superadmin|admin|inventory']], function () 
     Route::get('/search', 'SearchController@search')->name('search');
 });
 
-Route::group(['middleware' => ['role:superadmin|admin|cashier|inventory']], function () {
+Route::group(['middleware' => ['role:superadmin|admin|cashier|inventory', 'token.expired']], function () {
     Route::get('/admin', [HomeController::class, 'index'])->name('home');
     Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
     Route::post('/settings', [SettingController::class, 'store'])->name('settings.store');
     Route::resource('customers', CustomerController::class);
 });
 
-Route::group(['middleware' => ['role:superadmin']], function () {
+Route::group(['middleware' => ['role:superadmin', 'token.expired']], function () {
     Route::resource('users', UserController::class);
 });
