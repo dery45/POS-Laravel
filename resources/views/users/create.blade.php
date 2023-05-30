ef03211a01ddd8f2ed96bab66f46f75e9c2a2001
dery45 <deryap.dap@gmail.com>
2023-05-30 21:10:56 +0700

    fix create user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -24,6 +24,10 @@
                     <input type="password" name="password" id="password" class="form-control" required>
                 </div>
                 <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+                <div class="form-group">
                     <label for="role">Role</label>
                     <select name="role" id="role" class="form-control" required>
                         <option value="">Select Role</option>
