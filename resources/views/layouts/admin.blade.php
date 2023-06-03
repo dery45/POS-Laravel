e45647687d85358852afb6c5ec0e2406c6d0140b
bayu72928 <ammar07367@gmail.com>
2023-06-03 14:02:44 +0700

    offline

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,11 +8,11 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- overlayScrollbars -->
-    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/ionicons.min..css') }}"> -->
     <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <link href="{{ asset('css/css.css') }}" rel="stylesheet">
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @yield('css')
@@ -66,7 +66,7 @@
     </div>
     <!-- ./wrapper -->
     <!-- <script src="{{ asset('js/app.js') }}"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
     @yield('js')
 </body>
 
