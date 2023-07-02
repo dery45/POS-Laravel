4dea15bfd268788a47c3665bab7d3e28476c8185
dery45 <deryap.dap@gmail.com>
2023-07-02 13:02:37 +0700

    fix route on search and autocomplete

--- Backend/app_runner.py
+++ Backend/app_runner.py
@@ -4,21 +4,13 @@ import webbrowser
 import time
 
 # Command 1
-<<<<<<< HEAD
 directory1 = r"C:\xampp"
-=======
-directory1 = r"C:\XAMPP"
->>>>>>> 756270cc04c979fb4590fc9deb4dc382e5613da8
 command1 = 'xampp_start.exe'
 os.chdir(directory1)
 subprocess.call(command1, shell=True)
 
 # Command 2
-<<<<<<< HEAD
 directory2 = r'C:\xampp\htdocs\POS-Laravel'
-=======
-directory2 = r'C:\xampp\htdocs\pos-project\POS-Laravel'
->>>>>>> 756270cc04c979fb4590fc9deb4dc382e5613da8
 command2 = 'npm run dev'
 os.chdir(directory2)
 subprocess.Popen(['cmd', '/k', command2], shell=True)
@@ -28,21 +20,13 @@ command3 = 'php artisan serve'
 subprocess.Popen(['cmd', '/k', command3], shell=True)
 
 # Command 4
-<<<<<<< HEAD
 directory4 = r"C:\xampp\htdocs\POS-Laravel\Backend\venv\Scripts"
-=======
-directory4 = r"C:\xampp\htdocs\pos-project\POS-Laravel\Backend"
->>>>>>> 756270cc04c979fb4590fc9deb4dc382e5613da8
 command4 = 'activate.bat && cd ../.. && waitress-serve --port=5550 rekap_harian:app'
 os.chdir(directory4)
 subprocess.Popen(['cmd', '/k', command4], shell=True)
 
 # Command 5
-<<<<<<< HEAD
 directory5 = r"C:\xampp\htdocs\POS-Laravel\Backend\venv\Scripts"
-=======
-directory5 = r"C:\xampp\htdocs\pos-project\POS-Laravel\Backend"
->>>>>>> 756270cc04c979fb4590fc9deb4dc382e5613da8
 command5 = 'activate.bat && cd ../.. && waitress-serve --port=5551 dashboard:app'
 os.chdir(directory5)
 subprocess.Popen(['cmd', '/k', command5], shell=True)
